19b1160f41dad4ab437c717bcc4765dea667e300
Christopher Dudley <chrisdudley.inchrist@gmail.com>
2019-07-10 00:32:30 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int n = 3;
+        for (int[] array : allPossibleDiceRolls(n)) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(array[i] + " ");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static int[][] allPossibleDiceRolls(int numRolls) {
+        List<List<Integer>> allSolutions = new LinkedList<>();
+        for (int i = 1; i <= 6; i++) {
+            List<Integer> rollArray = new LinkedList<>();
+            allPossibleDiceRollsHelper(numRolls, rollArray, allSolutions);
+        }
+        return null;
+    }
+
+    public static void allPossibleDiceRollsHelper(int rollsRemaining, List<Integer> arr, List<List<Integer>> allSolutions) {
+
+
+        if (rollsRemaining == 0) {
+            List<Integer> arrayAdd = arr;
+            allSolutions.add(arrayAdd);
+        } else {
+            for (int i = 1; i <= 6; i++) {
+                arr.add(i);
+                allPossibleDiceRollsHelper(rollsRemaining - 1, arr, allSolutions);
+
+                arr.remove(i);
+            }
+        }
+    }
+}
